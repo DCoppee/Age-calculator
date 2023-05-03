24cac4e5ac306732824509829e8845cd1c8365a8
DCoppee <dominiquecoppee.pro@gmail.com>
2023-05-03 12:34:19 +0200

    functions js

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,85 @@
 function calculateAge() {
-  var today = new Date();
-  var birthdate = new Date(document.getElementById("birthdate").value);
-  var age = today.getFullYear() - birthdate.getFullYear();
-  var months = today.getMonth() - birthdate.getMonth();
-  if (months < 0 || (months === 0 && today.getDate() < birthdate.getDate())) {
-    age--;
-    months += 12;
+  var day = parseInt(document.getElementById("day").value);
+  var month = parseInt(document.getElementById("month").value);
+  var year = parseInt(document.getElementById("year").value);
+
+  var age = {
+    years: 0,
+    months: 0,
+    days: 0,
+  };
+
+  var error = "";
+
+  function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
   }
-  var days = Math.floor(
-    (today.getTime() - birthdate.getTime()) / (1000 * 60 * 60 * 24)
-  );
-  var ageDiv = document.getElementById("age");
-  var errorDiv = document.getElementById("error");
-  if (
-    isNaN(birthdate.getTime()) ||
-    document.getElementById("birthdate").value === ""
+
+  // Vérifier si les champs ne sont pas vides
+  if (!day || !month || !year) {
+    error = "Veuillez remplir tous les champs";
+  }
+  // Vérifier si les valeurs sont valides
+  else if (isNaN(day) || isNaN(month) || isNaN(year)) {
+    error = "Veuillez entrer des valeurs numériques valides";
+  }
+  // Vérifier si l'année est dans le futur
+  else if (year > new Date().getFullYear()) {
+    error = "L'année saisie ne peut pas être dans le futur";
+  }
+  // Vérifier si la date est valide
+  else if (
+    day <= 0 ||
+    day > 31 ||
+    month <= 0 ||
+    month > 12 ||
+    year <= 0 ||
+    year > new Date().getFullYear()
   ) {
-    ageDiv.innerHTML = "";
-    errorDiv.innerHTML = "Veuillez entrer une date valide.";
-  } else if (today < birthdate) {
-    ageDiv.innerHTML = "";
-    errorDiv.innerHTML = "Veuillez entrer une date antérieure à aujourd'hui.";
+    error = "Veuillez entrer une date valide";
   } else {
-    ageDiv.innerHTML =
-      "Vous avez " +
-      age +
-      " ans, " +
-      months +
-      " mois et " +
-      (days - (age * 365 + months * 30)) +
-      " jours.";
-    errorDiv.innerHTML = "";
+    // Calculer l'âge en années, mois et jours
+    var birthdate = new Date(new Date().getFullYear(), month - 1, day);
+    var today = new Date();
+    age.years = today.getFullYear() - year;
+    age.months = today.getMonth() - month;
+    age.days = today.getDate() - day;
+
+    // Gérer les cas où le nombre de mois ou de jours est négatif
+    if (age.months < 0) {
+      age.years--;
+      age.months += 12;
+    }
+
+    if (age.days < 0) {
+      age.months--;
+      age.days += daysInMonth(today.getMonth() + 1, today.getFullYear());
+    }
+
+    // Gérer le cas où l'utilisateur est né le jour même
+    if (
+      birthdate.getDate() === today.getDate() &&
+      birthdate.getMonth() === today.getMonth()
+    ) {
+      error = "Joyeux anniversaire!";
+    }
+  }
+
+  // Afficher le résultat ou le message d'erreur
+  if (error) {
+    document.getElementById("age").innerHTML = "";
+    document.getElementById("error").innerHTML = error;
+  } else {
+    document.getElementById("age").innerHTML =
+      age.years + " years, " + age.months + " months, " + age.days + " days";
+    document.getElementById("error").innerHTML = "";
   }
 }
-function calculateAge() {
-  var today = new Date();
-  var birthdate = new Date(document.getElementById("birthdate").value);
-  var age = today.getFullYear() - birthdate.getFullYear();
-  var months = today.getMonth() - birthdate.getMonth();
-  if (months < 0 || (months === 0 && today.getDate() < birthdate.getDate())) {
-    age--;
-    months += 12;
-  }
-  var days = Math.floor(
-    (today.getTime() - birthdate.getTime()) / (1000 * 60 * 60 * 24)
-  );
-  var ageDiv = document.getElementById("age");
-  var errorDiv = document.getElementById("error");
-  if (
-    isNaN(birthdate.getTime()) ||
-    document.getElementById("birthdate").value === ""
-  ) {
-    ageDiv.innerHTML = "";
-    errorDiv.innerHTML = "Veuillez entrer une date valide.";
-  } else if (today < birthdate) {
-    ageDiv.innerHTML = "";
-    errorDiv.innerHTML = "Veuillez entrer une date antérieure à aujourd'hui.";
-  } else {
-    ageDiv.innerHTML =
-      "Vous avez " +
-      age +
-      " ans, " +
-      months +
-      " mois et " +
-      (days - (age * 365 + months * 30)) +
-      " jours.";
-    errorDiv.innerHTML = "";
-  }
+
+function resetFields() {
+  document.getElementById("day").value = "";
+  document.getElementById("month").value = "";
+  document.getElementById("year").value = "";
+  document.getElementById("age").innerHTML = "";
+  document.getElementById("error").innerHTML = "";
 }
